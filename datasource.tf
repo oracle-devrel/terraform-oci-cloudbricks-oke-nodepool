ed0700aba4da58d0b97a0c036f4569ea181cfc46
dralquinta <dralquinta@gmail.com>
2021-11-03 20:01:51 +0000

    Updating regex for lookup of OS image

--- datasource.tf
+++ datasource.tf
@@ -54,7 +54,8 @@ data "oci_core_images" "OL79" {
   shape                    = var.node_pool_shape
   filter {
     name   = "display_name"
-    values = ["^([a-zA-z]+)-([a-zA-z]+)-([\\.0-9]+)-([\\.0-9-]+)$"]
+    #values = ["^([a-zA-z]+)-([a-zA-z]+)-([\\.0-9]+)-([\\.0-9-]+)$"]
+    values = ["^.*Oracle[^G]*$"]
     regex  = true
   }
 }
